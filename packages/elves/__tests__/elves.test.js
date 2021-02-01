818385ac12b01c0d074f167e48edc4a8ebad6356
Octo1996 <Octo1996@outlook.com>
2021-02-01 20:38:05 +0800

    chore(lerna): lerna init and gen docs elves

---
+++ packages/elves/__tests__/elves.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const elves = require('..');
+
+describe('elves', () => {
+    it('needs tests');
+});
+/* 
+packages
+  ├── docs
+  │    ├─ package.josn
+  │    └─ README.md
+  ├── elves
+  │    ├─ package.josn
+  │    └─ README.md
+  ├── lerna.json
+  ├── LICENSE
+  └── package.json
+*/
